167bc731a5007b95af11159ca939ed50df4c29b9
ismail <iharmanda@hotmail.com>
2022-02-07 13:47:22 +0300

    boilerplate

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { Text, View, StyleSheet } from "react-native";
+
+export default HomeScreen = () => {
+  return <Text>HomeScreen</Text>;
+};
+const styles = StyleSheet.create({});
